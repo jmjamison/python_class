51955b067a0df8828d9545d3f72c7907b2f353fd
Jamie Jamison <jamison@library.ucla.edu>
2021-10-22 12:06:46 -0700

    add program2part2.py and correct input file name

---
+++ python1_2021/week02/program2part2.py
@@ -0,0 +1,22 @@
+# week 3 python 1 class
+'''
+Name: Jamie Jamison
+Date: 2021-10-22
+Description:  program 2 part 2
+
+
+'''
+#import os
+
+#cwd = os.getcwd()
+#print("Current working directory: {0}".format(cwd))
+# use roster_extended.csv
+
+greeting = 'Hello.'
+
+print(greeting)
+
+roster_file = open(input("Please enter a roster file: "), "r")
+
+print(roster_file)
+
