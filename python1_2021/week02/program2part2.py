e560ae7282be1c6affdae5066f6651364f6eae48
Jamie Jamison <jamison@library.ucla.edu>
2021-10-22 12:46:28 -0700

    edits to program2part2

--- python1_2021/week02/program2part2.py
+++ python1_2021/week02/program2part2.py
@@ -6,6 +6,14 @@ Description:  program 2 part 2
 
 
 '''
+def add_people(x):
+    how_many = x
+    print("adding {0} names.".format(x))
+    
+    for num in range(0,int(how_many)):
+        print(num+1)
+        
+
 #import os
 
 #cwd = os.getcwd()
@@ -16,7 +24,20 @@ greeting = 'Hello.'
 
 print(greeting)
 
+# read-only is the default but set to be careful
 roster_file = open(input("Please enter a roster file: "), "r")
+# read the header first to get rid of it then read the content
+header = roster_file.readline()
+#print(header)
+roster_content = roster_file.readlines()
+roster_file.close()
 
-print(roster_file)
 
+#print(roster_content)
+
+num_names = len(roster_content)
+print("There are {0} names in this file. Would you like to enter additional names? (Y/N)".format(num_names))
+
+num_names = input("How many more names? ")
+
+add_people(num_names)
